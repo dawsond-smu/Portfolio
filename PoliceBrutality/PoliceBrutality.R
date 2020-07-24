766b3438daac0db4534ef7accd93f362d267c151
dawsond-smu <62529301+dawsond-smu@users.noreply.github.com>
2020-07-24 01:45:12 -0400

    Adding Test and Actual data

--- PoliceBrutality/PoliceBrutality.R
+++ PoliceBrutality/PoliceBrutality.R
@@ -24,6 +24,7 @@ library(readr)  # for read_csv
 library(knitr)  # for kable
 library(RCurl)
 
+#Test Upload and na.omit() Sample
 BeersUrl ="https://raw.githubusercontent.com/dawsond-smu/dawsond-smu.github.io/master/Data/Beers.csv"
 Beers <-read_csv(url(BeersUrl))
 BreweriesUrl ="https://raw.githubusercontent.com/dawsond-smu/dawsond-smu.github.io/master/Data/Breweries.csv"
@@ -32,6 +33,16 @@ Beers_Brewery = merge(Beers,Breweries, by.x = "Brewery_id", by.y = "Brew_ID")
 Beers_Brewery_Omit = na.omit(Beers_Brewery)
 
 
-PoliceUrl ="https://raw.githubusercontent.com/dawsond-smu/Analytics/master/PoliceBrutality/PoliceKillingsUS.csv"
+PoliceUrl ="https://raw.githubusercontent.com/dawsond-smu/Portfolio/master/PoliceBrutality/PoliceKillingsUS.csv"
 Police <-read_csv(url(PoliceUrl))
-head(Police)
+RaceUrl ="https://raw.githubusercontent.com/dawsond-smu/Portfolio/master/PoliceBrutality/ShareRaceByCity.csv"
+Race <-read_csv(url(RaceUrl))
+IncomeUrl ="https://raw.githubusercontent.com/dawsond-smu/Portfolio/master/PoliceBrutality/MedianHouseholdIncome2015.csv"
+Income <-read_csv(url(IncomeUrl))
+PovertyUrl ="https://raw.githubusercontent.com/dawsond-smu/Portfolio/master/PoliceBrutality/PercentagePeopleBelowPovertyLevel.csv"
+Poverty <-read_csv(url(PovertyUrl))
+EducationUrl ="https://raw.githubusercontent.com/dawsond-smu/Portfolio/master/PoliceBrutality/PercentOver25CompletedHighSchool.csv"
+Education <-read_csv(url(EducationUrl))
+
+
+
